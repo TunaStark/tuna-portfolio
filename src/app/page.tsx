af7badbac98af25ee664f452589eec1f4c128630
TunaStark <guralptuna@gmail.com>
2026-01-19 11:13:36 +0300

    Edited layout and main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import Hero from "@/components/Hero";
-import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
-      <main className="pt-20">
+      <main>
         <Hero />
       </main>
     </>
